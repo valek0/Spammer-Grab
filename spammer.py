2bf196b685b9515dcce48007d8a11982e4e7b0e8
root <root@localhost.localdomain>
2018-01-06 05:54:08 -0500

    Added few new features

--- spammer.py
+++ spammer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python
 
-# - Spammer v2
+# - Spammer v3.0
 # | Description: spams a phone number by sending it a lot of sms by using Grab API
 # | Author: P4kL0nc4t
-# | Date: 5/12/2017
+# | Date: 06/01/2018
 # | Disclaimer: Editing author will not make you the real coder
+# | What's New?
+# | - Added coloring
+# | - Added support multiple proxies (BETA)
+# | - Added feature to create a new separate thread for each proxy in order to increase spam speed (BETA)
+# | - Added support for phone number with +xxxxxx format 
+# | - And another fixes
 
 import spammer_class
 spammer = spammer_class.Spammer()
@@ -12,5 +18,5 @@ spammer.author = "P4kL0nc4t"
 try:
     spammer.main()
 except KeyboardInterrupt:
-    print "\r[!][except] KeyboardInterrupt detected! Exiting . . ."
+    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
     exit()
