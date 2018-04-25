04c4236cd1f81daed7cee236bdcbc461ff826ec2
Noxturnix <folkgm@hotmail.com>
2018-04-25 21:27:50 +0700

    Fixed 403 forbidden

--- spammer.py
+++ spammer.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python
 
-# - Spammer v3.0
+# - Spammer v3.1
 # | Description: spams a phone number by sending it a lot of sms by using Grab API
-# | Author: P4kL0nc4t
-# | Date: 06/01/2018
+# | Author: P4kL0nc4t & Noxturnix
+# | Date: 25/04/2018
 # | Disclaimer: Editing author will not make you the real coder
 # | What's New?
-# | - Added coloring
-# | - Added support multiple proxies (BETA)
-# | - Added feature to create a new separate thread for each proxy in order to increase spam speed (BETA)
-# | - Added support for phone number with +xxxxxx format 
-# | - And another fixes
+# | - Fixed 403 forbidden
+# | - Use less CPU
 
 import spammer_class
 spammer = spammer_class.Spammer()
-spammer.author = "P4kL0nc4t"
+spammer.author = "P4kL0nc4t & Noxturnix"
 try:
     spammer.main()
 except KeyboardInterrupt:
-    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC
+    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC+"\n"
     exit()
